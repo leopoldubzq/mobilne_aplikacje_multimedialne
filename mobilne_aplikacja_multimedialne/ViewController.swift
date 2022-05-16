17ef705cac60b0869614afd7c62e2f8b3a543c5c
leopold.romanowski <leopold.romanowski@itcraft.pl>
2022-05-16 16:41:43 +0200

    Initial Commit

---
+++ mobilne_aplikacja_multimedialne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mobilne_aplikacja_multimedialne
+//
+//  Created by Leopold Romanowski on 16/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
